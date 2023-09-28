2792e6866c485d2123854809c9836a3edf3f8a25
Sujith M <sujithm1610@gmail.com>
2023-09-28 16:30:45 +0530

    app run

--- MLproject/config/configuration.py
+++ MLproject/config/configuration.py
@@ -99,7 +99,7 @@ class ConfigurationManager:
             all_params=params,
             metric_file_name = config.metric_file_name,
             target_column = schema.name,
-            mlflow_uri="https://dagshub.com/entbappy/End-to-end-Machine-Learning-Project-with-MLflow.mlflow",
+            mlflow_uri="https://dagshub.com/sujithm16/mlproject01.mlflow",
            
         )
 
