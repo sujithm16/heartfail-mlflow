fe4f4dbe426092cdb26c5386d7beece07a2fb023
Sujith M <sujithm1610@gmail.com>
2023-09-29 11:04:05 +0530

    run pass

--- heartfailure/pipeline/stage3_data_transformation.py
+++ heartfailure/pipeline/stage3_data_transformation.py
@@ -14,22 +14,10 @@ class DataTransformationTrainingPipeline:
 
 
     def main(self):
-        try:
-            with open(Path("artifacts/data_validation/status.txt"), "r") as f:
-                status = f.read().split(" ")[-1]
-
-            if status == "True":
-                config = ConfigurationManager()
-                data_transformation_config = config.get_data_transformation_config()
-                data_transformation = DataTransformation(config=data_transformation_config)
-                data_transformation.train_test_spliting()
-
-            else:
-                raise Exception("You data schema is not valid")
-
-        except Exception as e:
-            print(e)
-
+        config = ConfigurationManager()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.train_test_spliting()
 
 
 
